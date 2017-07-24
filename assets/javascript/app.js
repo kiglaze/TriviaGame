09c43e29a45aecf5b181b1b6341ef137571fe539
Kristin Glaze <ikglaze33@gmail.com>
2017-07-24 00:34:56 -0400

    Code is able to tally up score, and also had an implemented timer.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-	var correctAnswers = 100;
-	var incorrectAnswers = 4;
-	var unansweredAnswers = 1;
+	var correctAnswersCount = 0;
+	var incorrectAnswersCount = 0;
+	var unansweredAnswersCount = 0;
+	
+	var secondsLeft = 30;
+
+	var answerKey = {
+		1: "The Lion King",
+		2: "The Lion King"
+	};
 	$(".start").click(function() {
 		startGame();
 	});
@@ -9,17 +16,42 @@ $(document).ready(function() {
 		endGame();
 	});
 	function startGame() {
+		debugger;
+		var timeRemainingHandle = setInterval(function() {
+			secondsLeft--;
+			$(".time-remaining").text(secondsLeft);
+			if(secondsLeft <= 0) {
+				clearInterval(timeRemainingHandle);
+				endGame();
+			}
+		}, 1000);
 		$(".start").hide();
 		$(".trivia-game-view").show();
 	}
 	function endGame() {
 		$(".trivia-game-view").hide();
+		tallyScores();
+		fillResultsTable(correctAnswersCount, incorrectAnswersCount, unansweredAnswersCount);
 		$(".results").show();
-		fillResultsTable(correctAnswers, incorrectAnswers, unansweredAnswers);
 	}
 	function fillResultsTable(correctCount, incorrectCount, unansweredCount) {
 		$("table.results-table tr.correct td").text(correctCount);
 		$("table.results-table tr.incorrect td").text(incorrectCount);
 		$("table.results-table tr.unanswered td").text(unansweredCount);
+	}
+	function tallyScores() {
+		$(".trivia-question").each(function(key, value) {
+			var $chosenInputRadio = $(value).find("input:checked");
+			var expectedAnswer = answerKey[key + 1];
+			if(typeof expectedAnswer !== "undefined") {
+				if(expectedAnswer == $chosenInputRadio.val()) {
+					correctAnswersCount++;
+				} else if($chosenInputRadio.length == 0) {
+					unansweredAnswersCount++;
+				} else {
+					incorrectAnswersCount++;
+				}
+			}
+		});
 	}
 });
